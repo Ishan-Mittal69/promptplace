ed6da587eb5b39a2a3e61e1e4047a907c72d98de
Ishan-Mittal69 <mittalishan169@gmail.com>
2024-06-30 11:52:16 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,38 @@
+ import '@styles/global.css';
+ import Nav from "@components/Nav";
+ import Provider from "@components/Provider";
+
+
+ export const metadata={
+  title: "PromptPlace",
+  description: "Discover share and test AI prompts! "
+ }
+
+ 
+ function Rootlayout({children}) {
+   return (
+    <html lang='en'>
+      
+      <body>
+        <Provider>
+
+        <div className='main'>
+         
+          <div className='gradient'/>
+        
+        </div>
+
+        <main className='app'>
+          <Nav/>
+          {children}
+        </main>
+
+        </Provider>
+
+      </body>
+    </html>
+   )
+ }
+ 
+ export default Rootlayout
+
---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+//we dont need to import reacct in next js
+import Feed from "@components/Feed"; 
+
+function Home() {
+  return (
+    <section className="w-full flex-center flex-col"> 
+      <h2 className="head_text text-center" > 
+      Discover & Share 
+      </h2 >
+      <h1 className="orange_gradient text-7xl text-center max-md:text-6xl max-sm:text-5xl"> AI-Powered Prompts</h1>
+       
+      <p className="desc text-center">PromptPlace is a place as its name suggest for prompts where you can discover, create , share and test prompts!</p>
+
+      <Feed/> 
+    </section>
+  )
+}
+
+export default Home
